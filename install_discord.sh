dd09f1221e2400265d047f98b82a8e2e28697273
Arthur Phillip D. Silva <artphil.dev@gmail.com>
2024-08-14 08:41:06 -0300

    Update install_discord.sh

--- install_discord.sh
+++ install_discord.sh
@@ -7,17 +7,27 @@
 
 SOURCE_FILE=discord.deb
 
+# Check if is super user
+if [ $(id -u) -ne 0 ] 
+  then echo "Please run as root"
+  exit
+fi
+
 # Go to the temp folder
 cd /tmp
 
 # Download the latest Discord source archive
+echo "Dowload files ..."
 wget 'https://discord.com/api/download?platform=linux&format=deb' -O $SOURCE_FILE
 
 # Install Discord
+echo "Installing ..."
 apt install ./$SOURCE_FILE
 
 # Remove source archive
-rm SOURCE_FILE
+echo "Removing files ..."
+rm $SOURCE_FILE
 
-# Go back home
-cd
+# Go back
+echo "Done"
+cd ..
